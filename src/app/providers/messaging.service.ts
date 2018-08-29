42a18b79a01e1a21a111061a86d9f43a41a8ae68
andrea leo <andrea.leo83@gmail.com>
2018-08-29 10:27:42 +0200

    reorder

--- src/app/providers/messaging.service.ts
+++ src/app/providers/messaging.service.ts
@@ -327,9 +327,34 @@ export class MessagingService {
           that.messages.push(msg);
         }
 
+        that.messages.sort(that.compareValues('timestamp', 'asc'));
+
+
       }
     });
   }
+
+  // function for dynamic sorting
+ private compareValues(key, order = 'asc') {
+  return function(a, b) {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+      // property doesn't exist on either object
+      return 0;
+    }
+    const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+    const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+
+    let comparison = 0;
+    if (varA > varB) {
+      comparison = 1;
+    } else if (varA < varB) {
+      comparison = -1;
+    }
+    return (
+      (order === 'desc') ? (comparison * -1) : comparison
+    );
+  };
+}
 
   /**
    * arriorno lo stato del messaggio
