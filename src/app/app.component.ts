8c07eec7d69b3fa00e5f6d58bd7b836ce0777749
andrea leo <andrea.leo83@gmail.com>
2018-12-07 16:23:11 +0100

    log fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -485,7 +485,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
         const messageSent = this.messagingService
             .sendMessage(senderFullname, text, type, '', recipient, recipientFullname, attributes, null, channel_type);
-        console.log('messageSent', messageSent);
+            this.g.wdLog(['messageSent', messageSent]);
     }
     /** */
 
@@ -508,7 +508,7 @@ export class AppComponent implements OnInit, OnDestroy {
                         that.authService.authenticateFirebaseCustomToken(firebaseToken);
                     }, error => {
                         console.error('Error decoding token: ', error);
-                        console.log('call signout');
+                       // console.log('call signout');
                         that.signOut();
                     });
                     // , () => {
@@ -516,8 +516,7 @@ export class AppComponent implements OnInit, OnDestroy {
                     // });
             }, error => {
                 console.error('Error creating firebase token: ', error);
-                console.error('Error decoding token: ', error);
-                console.log('call signout');
+                // console.log('call signout');
                 that.signOut();
             });
             // , () => {
