02cf68e73cfe2aec5d30baad1c2589ca2e40b611
andrea leo <andrea.leo83@gmail.com>
2019-03-27 13:24:58 +0100

    listerner

--- server.js
+++ server.js
@@ -13,4 +13,6 @@ res.sendFile(path.join(__dirname,'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+var listener = app.listen(process.env.PORT || 4200, function(){
+    console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+});
