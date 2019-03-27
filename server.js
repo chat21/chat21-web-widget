a0cd6885aeb3488e502aee6f58b4c3d1aa05862b
andrea leo <andrea.leo83@gmail.com>
2019-03-27 13:41:21 +0100

    fix

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/', function(req,res) {
-    
+app.get('/*', function(req,res) {
+    console.log("other res");   
     res.sendFile(path.join(__dirname,'/dist/index.html'));
 });
 
