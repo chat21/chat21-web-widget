cf60a0566f9e4a5b9eaec1e868db7ab82da04bc1
Dario De Pascalis <dariodepa75@gmail.com>
2019-07-10 11:05:47 +0200

    add config firebase pre

--- src/environments/environment.pre.ts
+++ src/environments/environment.pre.ts
@@ -2,19 +2,20 @@
 // The build2 system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build2 --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-import { firebaseConfig } from '../environments/firebase-config'; // please comment on this line when changing the values ​​of firebase {}
+// tslint:disable-next-line:max-line-length
+import { firebasePreConfig } from '../environments/firebase-config'; // please comment on this line when changing the values ​​of firebase {}
 
 export const environment = {
   production: true,
   version: require('../../package.json').version, // https://stackoverflow.com/questions/34907682/how-to-display-app-version-in-angular2
   remoteConfig: false,
   firebase: {
-    apiKey: firebaseConfig.apiKey,
-    authDomain: firebaseConfig.authDomain,
-    databaseURL: firebaseConfig.databaseURL,
-    projectId: firebaseConfig.projectId,
-    storageBucket: firebaseConfig.storageBucket,
-    messagingSenderId: firebaseConfig.messagingSenderId
+    apiKey: firebasePreConfig.apiKey,
+    authDomain: firebasePreConfig.authDomain,
+    databaseURL: firebasePreConfig.databaseURL,
+    projectId: firebasePreConfig.projectId,
+    storageBucket: firebasePreConfig.storageBucket,
+    messagingSenderId: firebasePreConfig.messagingSenderId
   },
   // apiUrl: 'https://api.tiledesk.com/v1/',
   apiUrl: 'https://tiledesk-server-pre.herokuapp.com/',
