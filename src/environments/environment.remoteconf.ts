620ca4cb59c93bfbf19d6148bb73cd59513776b6
Andrea Leo <andrealeo83@users.noreply.github.com>
2019-08-28 12:37:13 +0200

    Create environment.remoteconf.ts

---
+++ src/environments/environment.remoteconf.ts
@@ -0,0 +1,33 @@
+// The file contents for the current environment will overwrite these during build2.
+// The build2 system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build2 --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+// tslint:disable-next-line:max-line-length
+// import { firebaseConfig } from '../environments/firebase-config'; // please comment on this line when changing the values ​​of firebase {}
+
+export const environment = {
+  production: false,
+  version: require('../../package.json').version, // https://stackoverflow.com/questions/34907682/how-to-display-app-version-in-angular2
+  remoteConfig: true,
+  remoteConfigUrl: 'http://localhost:3000/settings',
+  firebase: {
+    apiKey: 'CHANGEIT',
+    authDomain: 'CHANGEIT',
+    databaseURL: 'CHANGEIT',
+    projectId: 'CHANGEIT',
+    storageBucket: 'CHANGEIT',
+    messagingSenderId: 'CHANGEIT'
+
+    // apiKey: firebaseConfig.apiKey,
+    // authDomain: firebaseConfig.authDomain,
+    // databaseURL: firebaseConfig.databaseURL,
+    // projectId: firebaseConfig.projectId,
+    // storageBucket: firebaseConfig.storageBucket,
+    // messagingSenderId: firebaseConfig.messagingSenderId
+  },
+  apiUrl: 'https://api.tiledesk.com/v1/',
+  tenant: 'tilechat',
+  defaultLang : 'en',
+  shemaVersion : '1'
+};
