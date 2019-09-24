024d321d79e48156dc5f6d2cda131c119096509f
Dario De Pascalis <dariodepa75@gmail.com>
2019-09-24 10:39:46 +0200

    no message

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,12 +12,19 @@ export const environment = {
   remoteConfig: true,
   remoteConfigUrl: '/firebase-config.json',
   firebase: {
-    apiKey: 'CHANGEIT',
-    authDomain: 'CHANGEIT',
-    databaseURL: 'CHANGEIT',
-    projectId: 'CHANGEIT',
-    storageBucket: 'CHANGEIT',
-    messagingSenderId: 'CHANGEIT'
+    // apiKey: 'CHANGEIT',
+    // authDomain: 'CHANGEIT',
+    // databaseURL: 'CHANGEIT',
+    // projectId: 'CHANGEIT',
+    // storageBucket: 'CHANGEIT',
+    // messagingSenderId: 'CHANGEIT'
+
+    apiKey: 'AIzaSyDWMsqHBKmWVT7mWiSqBfRpS5U8YwTl7H0',
+    authDomain: 'chat-v2-dev.firebaseapp.com',
+    databaseURL: 'https://chat-v2-dev.firebaseio.com',
+    projectId: 'chat-v2-dev',
+    storageBucket: 'chat-v2-dev.appspot.com',
+    messagingSenderId: '77360455507'
 
     // apiKey: firebaseConfig.apiKey,
     // authDomain: firebaseConfig.authDomain,
