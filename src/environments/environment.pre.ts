0ba89f664fb5afcc4a3475498b66e47032247cac
nicola74 <nicola.lanzilotto@frontiere21.it>
2020-12-29 13:10:37 +0100

    remote config true

--- src/environments/environment.pre.ts
+++ src/environments/environment.pre.ts
@@ -9,7 +9,7 @@ export const environment = {
   production: true,
   version: require('../../package.json').version, // https://stackoverflow.com/questions/34907682/how-to-display-app-version-in-angular2
 
-  remoteConfig: false,
+  remoteConfig: true,
   remoteConfigUrl: '/widget-config.json',
   loadRemoteTranslations: true,
   remoteTranslationsUrl: 'https://tiledesk-server-pre.herokuapp.com/',
