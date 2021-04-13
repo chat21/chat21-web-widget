2146ab6ea71379f5c62af511ec698a839d33effb
andrea leo <andrea.leo83@gmail.com>
2021-04-13 16:27:37 +0200

    env prod fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,17 @@
-export const environment = {
-  production: true,
-  version: require('../../package.json').version, // https://stackoverflow.com/questions/34907682/how-to-display-app-version-in-angular2
+// The file contents for the current environment will overwrite these during build2.
+// The build2 system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build2 --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
 
-  remoteConfig: true, // for performance don't load settings from remote
+export const environment = {
+  production: false,
+  version: require('../../package.json').version, // https://stackoverflow.com/questions/34907682/how-to-display-app-version-in-angular2
+  remoteConfig: true,
   remoteConfigUrl: '/widget-config.json',
   loadRemoteTranslations: true,
-  remoteTranslationsUrl: 'https://api.tiledesk.com/v2/',
-
-  firebase: {
+  remoteTranslationsUrl: 'http://localhost:3000/',
+  chatEngine: 'mqtt',
+  firebaseConfig: {
     apiKey: 'CHANGEIT',
     authDomain: 'CHANGEIT',
     databaseURL: 'CHANGEIT',
@@ -15,8 +19,17 @@ export const environment = {
     storageBucket: 'CHANGEIT',
     messagingSenderId: 'CHANGEIT'
   },
-  apiUrl: 'https://api.tiledesk.com/v2/',
+  chat21Config: {
+    appId: "tilechat",
+    MQTTendpoint: 'mqtt://localhost:15675/ws', // MQTT endpoint
+    APIendpoint: 'http://localhost:8004/api',
+    loginServiceEndpoint: 'http://localhost:3000/chat21/native/auth/createCustomToken' // endpoint readed from apiUrl property
+  },
+  apiUrl: 'http://localhost:3000/',
+  baseImageUrl: 'https://firebasestorage.googleapis.com/v0/b/',
   tenant: 'tilechat',
   defaultLang : 'en',
-  shemaVersion : '4'
+  shemaVersion : '1',
+  authPersistence: 'LOCAL',
+  supportMode: true,
 };
