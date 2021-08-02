62054a6e9ad5f051fa97e55813b33714c9a1b5da
Gabriele Panico <gabriele.panico95@gmail.com>
2021-08-02 11:59:39 +0200

    new tiledesk poweredBy image logo

--- src/app/utils/globals.ts
+++ src/app/utils/globals.ts
@@ -26,7 +26,7 @@ export class Globals {
   BASE_LOCATION = 'https://widget.tiledesk.com/v2';
   // POWERED_BY = '<a tabindex="-1" target="_blank" href="http://www.tiledesk.com/">Powered by <b>TileDesk</b></a>';
   
-  POWERED_BY ='<a tabindex="-1" target="_blank" href="http://www.tiledesk.com/"><span>POWERED BY</span> <img src="https://tiledesk.com/wp-content/uploads/2020/08/tiledesk-logo_x4_vpadding.png"/></a>'
+  POWERED_BY ='<a tabindex="-1" target="_blank" href="http://www.tiledesk.com/"><span>POWERED BY</span> <img src="https://support-pre.tiledesk.com/dashboard/assets/img/logos/tiledesk-logo.svg"/></a>'
   DEFAULT_LOGO_CHAT = '/assets/images/tiledesk_logo_white_small.png';
   WIDGET_TITLE = 'Tiledesk';
 
