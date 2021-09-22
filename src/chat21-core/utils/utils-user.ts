2b1e404c313a3d82b2a6bcb9830c2981f4807319
Gabriele Panico <gabriele.panico95@gmail.com>
2021-09-22 13:11:24 +0200

    user-avatar color changed

--- src/chat21-core/utils/utils-user.ts
+++ src/chat21-core/utils/utils-user.ts
@@ -3,7 +3,7 @@
  * @param str
  */
 export function getColorBck(str: string): string {
-    const arrayBckColor = ['#fba76f', '#80d066', '#73cdd0', '#ecd074', '#6fb1e4', '#f98bae'];
+    const arrayBckColor = ['#E17076', '#7BC862', '#65aadd', '#a695e7', '#ee7aae', '#6ec9cb', '#faa774'];
     let num = 0;
     if (str) {
         const code = str.charCodeAt((str.length - 1));
