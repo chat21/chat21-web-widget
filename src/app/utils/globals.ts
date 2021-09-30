f54bc2119817785c4aacd9ad19c5e32ca06bcce3
Gabriele Panico <gabriele.panico95@gmail.com>
2021-09-30 09:13:10 +0200

    set default preChatForm value

--- src/app/utils/globals.ts
+++ src/app/utils/globals.ts
@@ -327,6 +327,9 @@ export class Globals {
     this.filterByRequester = false;
     /** show conversations with conversation.attributes.requester_id == user.uid */
     this.persistence = 'local';
+    /** set the auth persistence */
+    this.preChatFormJson = [{name: "userFullname22222", type:"string", mandatory:false, label:{ en:"User fullname", it:"Nome utente"}},{name:"userEmail", type:"string", mandatory:false, regex:"/^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)+$/", label:{ en:"Email", it: "Indirizzo email"},"errorLabel":{ pattern:{ en:"Invalid email address", it:"Indirizzo email non valido"}}}]
+    /** set the preChatForm Json as default if preChatFormCustomFieldsEnabled is false or not exist */
 
     this.showWaitTime = true;
 
