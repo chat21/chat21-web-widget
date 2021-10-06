0b6dfc158a4ab0f56fcf910a1ec44803fea7b628
Gabriele Panico <gabriele.panico95@gmail.com>
2021-10-06 09:56:11 +0200

    preChatForm updated

--- src/chat21-core/models/formArray.ts
+++ src/chat21-core/models/formArray.ts
@@ -1,13 +1,12 @@
 export class FormArray {
     constructor(
         public label?: string | {},
-        public description?: string | {},
         public errorLabel?: string | {},
         public name?: string,
         public type?: string,
         public mandatory?: boolean,
-        public regex?: string,
-        public value?: string,
+        public regex?: string,  
+        public value?: any,
         public options?: Array<any>
       ) { }
 }
