694c62e26f4784e99e7ec6b69fc521b44496eb8c
Gabriele Panico <gabriele.panico95@gmail.com>
2021-11-03 12:32:10 +0100

    added invertColor function to programatically decide the text color (white or black) of message text based on background color

--- src/chat21-core/utils/utils.ts
+++ src/chat21-core/utils/utils.ts
@@ -631,3 +631,37 @@ export function validateRegex(regex){
   }
   return validRegex
 }
+
+export function invertColor(hex: string, bw: boolean) {
+  let r,g,b;
+  if (hex.indexOf('#') === 0) {
+      hex = hex.slice(1);
+  }
+  // convert 3-digit hex to 6-digits.
+  if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  if (hex.length !== 6) {
+      throw new Error('Invalid HEX color.');
+  }
+  r = parseInt(hex.slice(0, 2), 16);
+  g = parseInt(hex.slice(2, 4), 16);
+  b = parseInt(hex.slice(4, 6), 16);
+
+  if (bw) {
+      // https://stackoverflow.com/a/3943023/112731
+      return (r * 0.299 + g * 0.587 + b * 0.114) > 186 ? '#000000' : '#FFFFFF';
+  }
+  // invert color components
+  r = (255 - r).toString(16);
+  g = (255 - g).toString(16);
+  b = (255 - b).toString(16);
+  // pad each with zeros and return
+  return "#" + padZero(r,2) + padZero(g,2) + padZero(b,2);
+}
+
+function padZero(str, len) {
+  len = len || 2;
+  var zeros = new Array(len).join('0');
+  return (zeros + str).slice(-len);
+}
